617e002b256b1c5f682139360732e21ee9835c24
nhi.ly <nhi.ly@be.com.vn>
2022-01-12 10:15:57 +0700

    setup repo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,10 @@
+import "@moai/core/dist/bundle.css";
+import "@moai/core/dist/font/remote.css";
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Layout from "../components/layout/Layout";
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <div>Nhi</div>
+    </Layout>
+  );
+};
+
+export default Home;
